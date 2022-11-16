29c1e190ab0e8e468301fe00cc6eb74a2caaec09
PA1NKII3R <84141021+PA1NK1LL3R-777@users.noreply.github.com>
2022-11-15 21:43:56 -0300

    Update telegram_backup-bot.sh

--- telegram_backup-bot.sh
+++ telegram_backup-bot.sh
@@ -2,6 +2,3 @@
 #backup para algum chat/grupo do telegram
 /bin/telegram -f "1496646298" "/root/bots" "bot1" "Backup feito as: $(date +%F\ %T)" >> /root/botbackup1.log
 
-#Removendo o arquivo de log do diretorio root
-
-rm -r botbackup1.log
